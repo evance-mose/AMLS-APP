3cc5a323741c59def7f7fc6fbbb4457bdd022dca
evance-mose <evasmose@gmail.com>
2025-11-02 19:53:54 +0200

    Refactor BaseUrl class to remove default API URL, allowing for environment variable configuration. This change enhances flexibility in managing base URLs through dotenv.

--- lib/services/base_url.dart
+++ lib/services/base_url.dart
@@ -2,6 +2,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class BaseUrl {
   static String get baseUrl {
-    return dotenv.env['BASE_URL'] ?? 'http://10.111.157.212:8000/api';
+    return dotenv.env['BASE_URL'] 
   }
 }
